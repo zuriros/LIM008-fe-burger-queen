b2d624456d2dd03ee5123f5fb06817d7de185e10
zuriros <hanami33rosas@gmail.com>
2019-10-23 16:48:47 -0500

    pruebas

--- data_firebase/realTime.js
+++ data_firebase/realTime.js
@@ -12,15 +12,15 @@
 import { initRouter } from '../src/hash_func.js';
 // Initialize Firebase
 const initFirebase = () => {
-const config = {
-  apiKey: 'AIzaSyAP8qgYLelKLSceXYf7_6JnRqI1xhPaomo',
-  authDomain: 'burger-queen-c806e.firebaseapp.com',
-  databaseURL: 'https://burger-queen-c806e.firebaseio.com',
-  projectId: 'burger-queen-c806e',
-  storageBucket: 'burger-queen-c806e.appspot.com',
-  messagingSenderId: '156052892794'
-};
-firebase.initializeApp(config);
-initRouter();
+  const config = {
+    apiKey: 'AIzaSyAP8qgYLelKLSceXYf7_6JnRqI1xhPaomo',
+    authDomain: 'burger-queen-c806e.firebaseapp.com',
+    databaseURL: 'https://burger-queen-c806e.firebaseio.com',
+    projectId: 'burger-queen-c806e',
+    storageBucket: 'burger-queen-c806e.appspot.com',
+    messagingSenderId: '156052892794'
+  };
+  firebase.initializeApp(config);
+  initRouter();
 };
 window.onload = initFirebase;
